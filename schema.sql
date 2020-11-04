abbf8cba3a31c6a9b4ae111afea47376cbf7aa7b
August Money <10575396+sudo-August@users.noreply.github.com>
2020-11-03 19:51:31 -0600

    database and websocket set up but need to be connected

--- schema.sql
+++ schema.sql
@@ -1,20 +1,20 @@
-﻿DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS transactions;
 
 CREATE TABLE transactions(
-	ID INT NOT NULL PRIMARY KEY,
+	id SERIAL NOT NULL PRIMARY KEY,
 	type TEXT NOT NULL, 
 	sequence INT NOT NULL, 
 	product_id TEXT NOT NULL, 
-	price FLOAT NOT NULL, 
-	open_24h FLOAT NOT NULL, 
-	volume_24h FLOAT NOT NULL, 
-	low_24h FLOAT NOT NULL, 
-	high_24h FLOAT NOT NULL, 
-	volume_30d FLOAT NOT NULL, 
-	best_bid FLOAT NOT NULL, 
-	best_ask FLOAT NOT NULL, 
+	price TEXT NOT NULL, 
+	open_24h TEXT NOT NULL, 
+	volume_24h TEXT NOT NULL, 
+	low_24h TEXT NOT NULL, 
+	high_24h TEXT NOT NULL, 
+	volume_30d TEXT NOT NULL, 
+	best_bid TEXT NOT NULL, 
+	best_ask TEXT NOT NULL, 
 	side TEXT NOT NULL, 
 	time TEXT, 
 	trade_id INT NOT NULL, 
-	last_size FLOAT NOT NULL
+	last_size TEXT NOT NULL
 );
